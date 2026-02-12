7f1916ca722f0cd57e4c0afd48db5a8fd728cea1
Krei <criketgun@gmail.com>
2026-02-12 17:43:03 +0800

    add faction helper functions

--- src/main/java/com/daragetsu/scgextra/Faction.java
+++ src/main/java/com/daragetsu/scgextra/Faction.java
@@ -2,6 +2,7 @@ package com.daragetsu.scgextra;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -26,5 +27,27 @@ public record Faction(String name, List<EntityType<?>> entities) {
 
     public static @Nullable Faction getFaction(String name) {
         return factions.get(name);
+    }
+
+    // Maybe cache or different structure to optimize this frequent call
+    public static @Nullable Faction getFaction(EntityType<?> entityType) {
+        return factions.values().stream()
+                .filter(faction -> faction.entities.contains(entityType))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static boolean isEnemies(LivingEntity entity1, LivingEntity entity2) {
+        Faction f1 = getFaction(entity1.getType());
+        Faction f2 = getFaction(entity2.getType());
+
+        return f1 != null && f2 != null && f1 != f2;
+    }
+
+    public static boolean isFriendlies(LivingEntity entity1, LivingEntity entity2) {
+        Faction f1 = getFaction(entity1.getType());
+        Faction f2 = getFaction(entity2.getType());
+
+        return f1 != null && f1 == f2;
     }
 }
