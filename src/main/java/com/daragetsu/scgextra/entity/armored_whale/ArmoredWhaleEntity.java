62323322076f775bd8ed4ffc81d45985da1409bc
Darad <nabbonoushad@gmail.com>
2026-02-16 05:18:42 +0600

    armored whale slam attack

--- src/main/java/com/daragetsu/scgextra/entity/armored_whale/ArmoredWhaleEntity.java
+++ src/main/java/com/daragetsu/scgextra/entity/armored_whale/ArmoredWhaleEntity.java
@@ -3,7 +3,9 @@ package com.daragetsu.scgextra.entity.armored_whale;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -38,6 +40,10 @@ public class ArmoredWhaleEntity extends Monster implements GeoEntity {
         this.goalSelector.addGoal(
             2, 
             new SlamAttackGoal(this)
+        );
+        this.goalSelector.addGoal(
+            2, 
+            new NearestAttackableTargetGoal<>(this, Player.class, true, false)
         );
     }
     
--- src/main/java/com/daragetsu/scgextra/entity/armored_whale/SlamAttackGoal.java
+++ src/main/java/com/daragetsu/scgextra/entity/armored_whale/SlamAttackGoal.java
@@ -1,7 +1,11 @@
 package com.daragetsu.scgextra.entity.armored_whale;
 
+import java.util.List;
 import java.util.Random;
 
+import com.daragetsu.scgextra.Faction;
+
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.phys.AABB;
 
@@ -21,18 +25,18 @@ public class SlamAttackGoal extends Goal{
         if(cooldown>0){
             --cooldown;
         }
-        return cooldown <= 0 && entity.getTarget() != null && random.nextInt(10)<4;
+        return cooldown <= 0 && entity.getTarget() != null && random.nextInt(100)<10;
     }
 
     @Override
     public void start() {
-        cooldown=600;
+        ticks = 0;
         super.start();
     }
 
     @Override
     public boolean canContinueToUse() {
-        return ticks <= 55 && entity.getTarget() != null;//~22 ticks for going up, ~22 ticks coming down and then apply the damage,the few ticks extra are just to be sure
+        return ticks <= 60 && entity.getTarget() != null && cooldown<=0;//~22 ticks for going up, ~22 ticks coming down and then apply the damage,the few ticks extra are just to be sure
     }
     @Override
     public void tick() {
@@ -41,32 +45,46 @@ public class SlamAttackGoal extends Goal{
         if(ticks<=20){
             entity.setDeltaMovement(
                 entity.getDeltaMovement().x(), 
-                0.5, 
+                0.3, 
                 entity.getDeltaMovement().z()
             );
-        }
-        if(entity.onGround()){
+        }else if(ticks > 20 && entity.onGround()){//only runs after the jump and once it's on the ground
             AABB fiveBlockRangeAABB = new AABB(
-                0,
-                0,
-                0,
-                0,
-                0,
-                0
+                entity.getX()-5,
+                entity.getY()-5,
+                entity.getZ()-5,
+                entity.getX()+5,
+                entity.getY()+5,
+                entity.getZ()+5
             );
             AABB twelveBlockRangeAABB = new AABB(
-                0,
-                0,
-                0,
-                0,
-                0,
-                0
+                entity.getX()-12,
+                entity.getY()-12,
+                entity.getZ()-12,
+                entity.getX()+12,
+                entity.getY()+12,
+                entity.getZ()+12
             );
-        }   
+            List<LivingEntity> entitiesInFiveBlocks = entity.level().getEntitiesOfClass(LivingEntity.class, fiveBlockRangeAABB, e -> !Faction.isFriendlies(entity, e));
+            List<LivingEntity> entitiesInTwelveBlocks = entity.level().getEntitiesOfClass(LivingEntity.class, twelveBlockRangeAABB, e -> !Faction.isFriendlies(entity, e));
+            // entities within 5 blocks get 30 damage,
+            // entities within 12 blocks get 10 damage,
+
+            // so i made the 5 block one deal 20 damage, and the 12 block one deals the remaining 10 damage to the 5 block range and the entities in the outer layer just gets the 10 damage, so i don't have to specifically make a outer box, yes I'm lazy
+
+            for(LivingEntity en : entitiesInFiveBlocks){
+                en.hurt(en.damageSources().mobAttack(entity), 20);
+            }
+            for(LivingEntity en : entitiesInTwelveBlocks){
+                en.hurt(en.damageSources().mobAttack(entity), 10);
+            }
+            this.stop();
+        }
     }
 
     @Override
     public void stop() {
+        cooldown=600;//start cooldown after the whole attack finishes
         ticks = 0;
     }
 }
