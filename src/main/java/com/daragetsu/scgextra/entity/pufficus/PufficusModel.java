708121d3e87e5bec527dc8dc92a6d84adcbc53cc
Krei <criketgun@gmail.com>
2026-02-25 10:10:05 +0800

    cleanup last

--- src/main/java/com/daragetsu/scgextra/entity/pufficus/PufficusModel.java
+++ src/main/java/com/daragetsu/scgextra/entity/pufficus/PufficusModel.java
@@ -1,9 +1,7 @@
 package com.daragetsu.scgextra.entity.pufficus;
 
 import com.daragetsu.scgextra.SCGExtra;
-import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
-import software.bernie.geckolib.model.GeoModel;
 
 public class PufficusModel<T extends PufficusEntity> extends DefaultedEntityGeoModel<T> {
 
--- src/main/java/com/daragetsu/scgextra/entity/turtleman/TurtlemanModel.java
+++ src/main/java/com/daragetsu/scgextra/entity/turtleman/TurtlemanModel.java
@@ -1,9 +1,7 @@
 package com.daragetsu.scgextra.entity.turtleman;
 
 import com.daragetsu.scgextra.SCGExtra;
-import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
-import software.bernie.geckolib.model.GeoModel;
 
 public class TurtlemanModel<T extends TurtlemanEntity> extends DefaultedEntityGeoModel<T> {
 
