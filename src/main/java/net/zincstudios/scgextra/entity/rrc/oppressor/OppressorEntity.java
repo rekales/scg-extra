65ab33779a92af688e8d513246fd7a69bba0b928
Krei <criketgun@gmail.com>
2026-03-17 04:04:27 +0800

    add oppressor data add oppressor initial goals

--- src/main/java/net/zincstudios/scgextra/entity/rrc/oppressor/OppressorEntity.java
+++ src/main/java/net/zincstudios/scgextra/entity/rrc/oppressor/OppressorEntity.java
@@ -3,19 +3,29 @@ package net.zincstudios.scgextra.entity.rrc.oppressor;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.zincstudios.scgextra.Faction;
+import net.zincstudios.scgextra.entity.common.ai.HurtByNonFactionGoal;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
+import top.ribs.scguns.config.EntityEquipmentConfig;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -32,8 +42,24 @@ public class OppressorEntity extends Monster implements GeoEntity {
     @SuppressWarnings("deprecation")
     @Override
     public @Nullable SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, @Nullable CompoundTag dataTag) {
-//        EntityEquipmentConfig.equipEntity(this, "scgextra:oppressor");  // NOTE: using raw string
+        EntityEquipmentConfig.equipEntity(this, "scgextra:oppressor");  // NOTE: using raw string
         return super.finalizeSpawn(level, difficulty, reason, spawnData, dataTag);
+    }
+
+    @Override
+    protected void registerGoals() {
+        // gun attack goal to be automatically added on finalizeSpawn
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+
+        // TODO: alert mechanic
+        // TODO: flare behaviour
+        this.targetSelector.addGoal(1, new HurtByNonFactionGoal(this));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true,
+                player -> !((Player) player).isCreative() && !player.isSpectator()));
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, LivingEntity.class, true,
+                entity -> Faction.isEnemies(this, entity)));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
@@ -41,7 +67,12 @@ public class OppressorEntity extends Monster implements GeoEntity {
                 .add(Attributes.MAX_HEALTH, 120D)
                 .add(Attributes.ARMOR, 6D)
                 .add(Attributes.FOLLOW_RANGE, 35.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.23F);
+                .add(Attributes.MOVEMENT_SPEED, 0.18F);
+    }
+
+    @Override
+    public boolean hurt(DamageSource source, float amount) {
+        return super.hurt(source, amount);
     }
 
     @Override
--- src/main/java/net/zincstudios/scgextra/entity/rrc/oppressor/OppressorRenderer.java
+++ src/main/java/net/zincstudios/scgextra/entity/rrc/oppressor/OppressorRenderer.java
@@ -8,6 +8,6 @@ import software.bernie.geckolib.renderer.GeoEntityRenderer;
 public class OppressorRenderer <T extends OppressorEntity> extends GeoEntityRenderer<T> {
 
     public OppressorRenderer(EntityRendererProvider.Context context) {
-        super(context, new DefaultedEntityGeoModel<>(SCGExtra.asResource("rrc/oppressor")));
+        super(context, new DefaultedEntityGeoModel<>(SCGExtra.asResource("rrc/oppressor"), false));
     }
 }
