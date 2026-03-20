6c3373ec560fae86109f4f31283509e320cac5ce
Krei <criketgun@gmail.com>
2026-03-20 22:48:13 +0800

    fix turtleman hitbox size add turtleman headshot hitbox

--- src/main/java/net/zincstudios/scgextra/entity/ModEntities.java
+++ src/main/java/net/zincstudios/scgextra/entity/ModEntities.java
@@ -37,6 +37,8 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import top.ribs.scguns.common.BoundingBoxManager;
+import top.ribs.scguns.common.headshot.BasicHeadshotBox;
 import top.ribs.scguns.entity.client.EnemyProjectileRenderer;
 
 public class ModEntities {
@@ -49,7 +51,7 @@ public class ModEntities {
     public static final RegistryObject<EntityType<TurtlemanEntity>> TURTLEMAN = ENTITY_TYPES
             .register("turtleman", () -> EntityType.Builder.of(TurtlemanEntity::new, MobCategory.MONSTER)
                     .updateInterval(1)
-                    .sized(0.6F, 1.95F)
+                    .sized(1F, 2.3F)
                     .build("turtleman"));
 
     public static final RegistryObject<EntityType<SalmonsaurEntity>> SALMONSAUR = ENTITY_TYPES
@@ -139,5 +141,9 @@ public class ModEntities {
         event.put(ModEntities.PUFFICUS.get(), PufficusEntity.createAttributes().build());
         event.put(ModEntities.ARMORED_WHALE.get(), ArmoredWhaleEntity.createAttributes().build());
         event.put(ModEntities.RAID_SUMMONER.get(), RaidSummonerEntity.createAttributes().build());
+    }
+
+    public static void registerHeadshotBoxes() {
+        BoundingBoxManager.registerHeadshotBox(ModEntities.TURTLEMAN.get(), new BasicHeadshotBox<>(10.0F, 20.0F));
     }
 }
