c2c2cb6bc00582bed5c7acf7a6894e74eb1e9759
Krei <criketgun@gmail.com>
2026-03-27 15:06:13 +0800

    disable remapping

--- src/main/java/net/zincstudios/scgextra/mixin/GunAttackGoalAccessor.java
+++ src/main/java/net/zincstudios/scgextra/mixin/GunAttackGoalAccessor.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import top.ribs.scguns.entity.ai.GunAttackGoal;
 
 // TODO: pr for additional methods for getting state and deconstructing tick into multiple methods.
-@Mixin(value = GunAttackGoal.class)
+@Mixin(value = GunAttackGoal.class, remap = false)
 public interface GunAttackGoalAccessor {
 
     @Accessor("aimingStabilityTimer")
