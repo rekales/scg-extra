3ec826c657a53791a622a92caf0d61384a45da4b
Krei <criketgun@gmail.com>
2026-03-28 22:42:34 +0800

    set update interval for flaming head to 1 add behavior state management

--- src/main/java/net/zincstudios/scgextra/entity/rrc/flaminghead/RammingAttackGoal.java
+++ src/main/java/net/zincstudios/scgextra/entity/rrc/flaminghead/RammingAttackGoal.java
@@ -1,19 +1,17 @@
 package net.zincstudios.scgextra.entity.rrc.flaminghead;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.Goal;
-import software.bernie.geckolib.animatable.GeoEntity;
 
 // Designed around FlamingHeadEntity, make generic later if needed using behaviour augmentation
 public class RammingAttackGoal extends Goal {
 
-    protected final PathfinderMob mob;
+    protected final FlamingHeadEntity mob;
     private final int cooldownDuration;
     private final float ramMaxDistance;
     private long cooldownEnd = 0;  // level timestamp
 
-    public RammingAttackGoal(PathfinderMob mob, int cooldownDuration, float ramMaxDistance) {
+    public RammingAttackGoal(FlamingHeadEntity mob, int cooldownDuration, float ramMaxDistance) {
         this.mob = mob;
         this.cooldownDuration = cooldownDuration;
         this.ramMaxDistance = ramMaxDistance;
@@ -31,11 +29,24 @@ public class RammingAttackGoal extends Goal {
     }
 
     @Override
+    public void stop() {
+        if (this.mob.getBehaviorState() == FlamingHeadEntity.BehaviorState.RAMMING) {
+            this.mob.setBehaviorState(FlamingHeadEntity.BehaviorState.NONE);
+        }
+    }
+
+    @Override
+    public boolean requiresUpdateEveryTick() {
+        return true;
+    }
+
+    @Override
     public void tick() {
         if (this.mob.level().getGameTime() > this.cooldownEnd) {
-            this.cooldownEnd = this.mob.level().getGameTime() + this.cooldownDuration;
-            if (this.mob instanceof GeoEntity geoEntity) {
-                geoEntity.triggerAnim("behaviour", "ram_attack");
+            if (this.mob.getBehaviorState() == FlamingHeadEntity.BehaviorState.NONE) {
+                this.cooldownEnd = this.mob.level().getGameTime() + this.cooldownDuration;
+                this.mob.setBehaviorState(FlamingHeadEntity.BehaviorState.RAMMING);
+                ramTarget();
             }
         }
     }
