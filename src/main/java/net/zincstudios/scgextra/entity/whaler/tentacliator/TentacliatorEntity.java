00da553642449aea0e4bfbbf22fbb86fc093f842
Krei <criketgun@gmail.com>
2026-03-30 21:25:11 +0800

    update and cleanup tentacliator generify trident attack goal

--- src/main/java/net/zincstudios/scgextra/entity/whaler/tentacliator/TentacliatorEntity.java
+++ src/main/java/net/zincstudios/scgextra/entity/whaler/tentacliator/TentacliatorEntity.java
@@ -1,5 +1,9 @@
 package net.zincstudios.scgextra.entity.whaler.tentacliator;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.monster.RangedAttackMob;
+import net.minecraft.world.entity.projectile.ThrownTrident;
+import net.minecraft.world.item.ItemStack;
 import net.zincstudios.scgextra.Faction;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
@@ -17,6 +21,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.zincstudios.scgextra.entity.common.GunnerEntity;
 import net.zincstudios.scgextra.entity.common.ai.HurtByNonFactionGoal;
 import net.zincstudios.scgextra.entity.common.ai.TridentAttackGoal;
 import software.bernie.geckolib.animatable.GeoEntity;
@@ -35,18 +40,18 @@ import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class TentacliatorEntity extends Drowned implements GeoEntity {
+public class TentacliatorEntity extends GunnerEntity implements GeoEntity, RangedAttackMob {
 
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    public TentacliatorEntity(EntityType<? extends Drowned> pEntityType, Level pLevel) {
+    public TentacliatorEntity(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public SpawnGroupData finalizeSpawn(ServerLevelAccessor pLevel, DifficultyInstance pDifficulty,
-        MobSpawnType pReason, @Nullable SpawnGroupData pSpawnData, @Nullable CompoundTag pDataTag) {
+    public @Nullable SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @org.jetbrains.annotations.Nullable SpawnGroupData spawnData, @org.jetbrains.annotations.Nullable CompoundTag dataTag) {
         EntityEquipmentConfig.equipEntity(this, "scgextra:tentacliator");  // NOTE: using raw string
-        return super.finalizeSpawn(pLevel, pDifficulty, pReason, pSpawnData, pDataTag);
+        return super.finalizeSpawn(level, difficulty, reason, spawnData, dataTag);
     }
 
     @Override
@@ -65,19 +70,18 @@ public class TentacliatorEntity extends Drowned implements GeoEntity {
     }
 
     @Override
-    protected void addBehaviourGoals() {
+    protected void registerGoals() {
         this.goalSelector.addGoal(1, new GunAttackGoal<>(this, this.getMainHandItem(), 1.0F, AIType.RECKLESS, 3));
+        this.goalSelector.addGoal(2, new InkAttackGoal(this));
+        this.goalSelector.addGoal(3, new TridentAttackGoal<>(this, 1.0D, 40, 10.0F));
         this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(9, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(2, new InkAttackGoal(this));
-        this.goalSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true, false));
 
-        this.targetSelector.addGoal(2, new HurtByNonFactionGoal(this));
+        this.targetSelector.addGoal(1, new HurtByNonFactionGoal(this));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true,
                 player -> !((Player) player).isCreative() && !player.isSpectator()));
         this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, LivingEntity.class, true,
                 entity -> Faction.isEnemies(this, entity)));
-        this.goalSelector.addGoal(2, new TridentAttackGoal(this, 1.0D, 40, 10.0F));
     }
     
     @Override
@@ -87,7 +91,7 @@ public class TentacliatorEntity extends Drowned implements GeoEntity {
 
     @Override
     public void registerControllers(ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "controller", 0, state -> {
+        controllers.add(new AnimationController<>(this, "controller", 2, state -> {
             if (state.isMoving()) {
                 state.setAndContinue(RawAnimation.begin().thenLoop("walk"));
             } else {
@@ -97,20 +101,14 @@ public class TentacliatorEntity extends Drowned implements GeoEntity {
         }));
     }
 
-    @Override
-    public boolean isBaby() {
-        return false;
-    }
-
-    @Override
-    protected boolean isSunSensitive() {
-        return false;
-    }     
-    @Override
-    public void tick() {
-        super.tick();
-        if(this.getMainHandItem().is(Items.FISHING_ROD)){
-            EntityEquipmentConfig.equipEntity(this, "scgextra:tentacliator");
-        }
+    public void performRangedAttack(LivingEntity target, float distanceFactor) {
+        ThrownTrident throwntrident = new ThrownTrident(this.level(), this, new ItemStack(Items.TRIDENT));
+        double d0 = target.getX() - this.getX();
+        double d1 = target.getY(0.3333333333333333D) - throwntrident.getY();
+        double d2 = target.getZ() - this.getZ();
+        double d3 = Math.sqrt(d0 * d0 + d2 * d2);
+        throwntrident.shoot(d0, d1 + d3 * (double)0.2F, d2, 1.6F, (float)(14 - this.level().getDifficulty().getId() * 4));
+        this.playSound(SoundEvents.DROWNED_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
+        this.level().addFreshEntity(throwntrident);
     }
 }
