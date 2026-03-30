eaad7f63ae29d48ba4ddbaeb7b43e7475329cbe7
Krei <criketgun@gmail.com>
2026-03-30 21:05:17 +0800

    update pufficus renderer remove redundant classes

--- src/main/java/net/zincstudios/scgextra/entity/whaler/pufficus/PufficusRenderer.java
+++ src/main/java/net/zincstudios/scgextra/entity/whaler/pufficus/PufficusRenderer.java
@@ -1,47 +1,17 @@
 package net.zincstudios.scgextra.entity.whaler.pufficus;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
-import software.bernie.geckolib.cache.object.GeoBone;
+import net.zincstudios.scgextra.SCGExtra;
+import net.zincstudios.scgextra.entity.common.client.GunGeoLayer;
+import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
-import software.bernie.geckolib.renderer.layer.BlockAndItemGeoLayer;
-
-import javax.annotation.Nullable;
 
 public class PufficusRenderer<T extends PufficusEntity> extends GeoEntityRenderer<T> {
 
     public PufficusRenderer(EntityRendererProvider.Context renderManager) {
-        super(renderManager, new PufficusModel<>());
+        super(renderManager, new DefaultedEntityGeoModel<>(SCGExtra.asResource("whaler/pufficus")));
 
-        addRenderLayer(new BlockAndItemGeoLayer<>(this) {
-            @Override
-            protected @Nullable ItemStack getStackForBone(GeoBone bone, PufficusEntity animatable) {
-                if (bone.getName().equals("right_hand")) {
-                    return animatable.getMainHandItem();
-                }
-                return null;
-            }
-
-            @Override
-            protected ItemDisplayContext getTransformTypeForStack(GeoBone bone, ItemStack stack, PufficusEntity animatable) {
-                if (bone.getName().equals("right_hand")) {
-                    return ItemDisplayContext.THIRD_PERSON_RIGHT_HAND;
-                }
-                return ItemDisplayContext.NONE;
-            }
-
-            @Override
-            protected void renderStackForBone(PoseStack poseStack, GeoBone bone, ItemStack stack, T animatable, MultiBufferSource bufferSource, float partialTick, int packedLight, int packedOverlay) {
-                if (bone.getName().equals("right_hand")) {
-                    poseStack.mulPose(Axis.XP.rotationDegrees(-115)); // Rotate X
-                }
-
-                super.renderStackForBone(poseStack, bone, stack, animatable, bufferSource, partialTick, packedLight, packedOverlay);
-            }
-        });
+        // Not actually gunner but I'll use it anyway
+        addRenderLayer(new GunGeoLayer<>(this, -25));
     }
 }
