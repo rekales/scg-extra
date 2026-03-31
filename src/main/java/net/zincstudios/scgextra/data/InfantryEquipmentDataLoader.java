af78bc45a0df68502780cad07b147837cf2fe411
Krei <criketgun@gmail.com>
2026-03-31 20:52:01 +0800

    remove dataloader

--- src/main/java/net/zincstudios/scgextra/data/InfantryEquipmentDataLoader.java
+++
@@ -1,9 +0,0 @@
-package net.zincstudios.scgextra.data;
-
-public class InfantryEquipmentDataLoader {
-    public String id;
-    public double equipment_chance;
-    public InfantryEquipmentItemLoader[] items;
-    public InfantryEquipmentDataLoader(){
-    }
-}
