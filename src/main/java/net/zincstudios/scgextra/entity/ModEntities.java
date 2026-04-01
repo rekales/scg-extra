dcc2ea6d3b070aadc3292e38dd27eb91caeb9eb4
Krei <criketgun@gmail.com>
2026-04-01 23:17:48 +0800

    tighter flaming head hitbox

--- src/main/java/net/zincstudios/scgextra/entity/ModEntities.java
+++ src/main/java/net/zincstudios/scgextra/entity/ModEntities.java
@@ -149,7 +149,7 @@ public class ModEntities {
 
     public static final RegistryObject<EntityType<FlamingHeadEntity>> FLAMING_HEAD = ENTITY_TYPES
             .register("flaming_head", () -> EntityType.Builder.of(FlamingHeadEntity::new, MobCategory.MONSTER)
-                    .sized(3.5F, 6F)
+                    .sized(2.6F, 5F)
                     .setUpdateInterval(1)
                     .build("flaming_head"));
 
