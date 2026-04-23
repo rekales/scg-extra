1bebdda8e033b9a9c3c59059fd07c13e72c87dcd
Krei <criketgun@gmail.com>
2026-04-23 16:46:15 +0800

    add scguns entity ai adjustments

--- src/main/java/net/zincstudios/scgextra/entity/EntityAdjustments.java
+++ src/main/java/net/zincstudios/scgextra/entity/EntityAdjustments.java
@@ -5,7 +5,7 @@ import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.zincstudios.scgextra.entity.common.ai.HurtByNonFactionGoal;
-import top.ribs.scguns.entity.monster.FinforcerEntity;
+import top.ribs.scguns.entity.monster.*;
 
 public class EntityAdjustments {
 
@@ -15,6 +15,35 @@ public class EntityAdjustments {
                     goal -> goal.getGoal() instanceof HurtByTargetGoal);
             mob.targetSelector.addGoal(1, new HurtByNonFactionGoal(mob));
             mob.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(mob, LivingEntity.class, true,
+                    entity -> Faction.isEnemies(mob, entity)));
+        }
+
+        if (event.getEntity() instanceof DissidentEntity mob) {
+            mob.targetSelector.getAvailableGoals().removeIf(
+                    goal -> goal.getGoal() instanceof HurtByTargetGoal);
+            mob.targetSelector.addGoal(1, new HurtByNonFactionGoal(mob));
+            mob.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(mob, LivingEntity.class, true,
+                    entity -> Faction.isEnemies(mob, entity)));
+        }
+        if (event.getEntity() instanceof AdjudicatorEntity mob) {
+            mob.targetSelector.getAvailableGoals().removeIf(
+                    goal -> goal.getGoal() instanceof HurtByTargetGoal);
+            mob.targetSelector.addGoal(1, new HurtByNonFactionGoal(mob));
+            mob.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(mob, LivingEntity.class, true,
+                    entity -> Faction.isEnemies(mob, entity)));
+        }
+        if (event.getEntity() instanceof SubjugatorEntity mob) {
+            mob.targetSelector.getAvailableGoals().removeIf(
+                    goal -> goal.getGoal() instanceof HurtByTargetGoal);
+            mob.targetSelector.addGoal(1, new HurtByNonFactionGoal(mob));
+            mob.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(mob, LivingEntity.class, true,
+                    entity -> Faction.isEnemies(mob, entity)));
+        }
+        if (event.getEntity() instanceof PraetorEntity mob) {
+            mob.targetSelector.getAvailableGoals().removeIf(
+                    goal -> goal.getGoal() instanceof HurtByTargetGoal);
+            mob.targetSelector.addGoal(1, new HurtByNonFactionGoal(mob));
+            mob.targetSelector.addGoal(5, new NearestAttackableTargetGoal<>(mob, LivingEntity.class, true,
                     entity -> Faction.isEnemies(mob, entity)));
         }
     }
