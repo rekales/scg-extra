c227e36c335f9573b3f4bfd9110ed7728905d6e4
Krei <criketgun@gmail.com>
2026-05-14 17:46:02 +0800

    cleanup assets remove redundant datagen code update generated assets

--- src/main/java/net/zincstudios/scgextra/datagen/ModItemModelProvider.java
+++ src/main/java/net/zincstudios/scgextra/datagen/ModItemModelProvider.java
@@ -17,8 +17,6 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        basicItem(ModItems.CAVALRY_SABER.get());
-        basicItem(ModItems.SPEAR_SHOVEL.get());
         basicItem(ModItems.WALKER_MG.get());
         basicItem(ModItems.END_SHELL.get());
 
