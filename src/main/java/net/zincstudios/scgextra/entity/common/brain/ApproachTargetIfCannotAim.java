b0fe026bfdb5d0bcdf49f6f2612a742193c2a0db
Krei <criketgun@gmail.com>
2026-06-19 15:53:16 +0800

    split ApproachTargetAndAim walk and aim logic

--- src/main/java/net/zincstudios/scgextra/entity/common/brain/ApproachTargetIfCannotAim.java
+++ src/main/java/net/zincstudios/scgextra/entity/common/brain/ApproachTargetIfCannotAim.java
@@ -3,7 +3,6 @@ package net.zincstudios.scgextra.entity.common.brain;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.Brain;
@@ -15,22 +14,19 @@ import net.minecraft.world.entity.ai.memory.WalkTarget;
 import net.zincstudios.scgextra.entity.ModBrainMemories;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
 import java.util.function.Function;
 
-import static net.zincstudios.scgextra.entity.ModBrainMemories.MAX_AIM_TICKS;
 import static net.zincstudios.scgextra.entity.ModBrainMemories.MIN_AIM_TICKS;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class ApproachTargetAndAim extends Behavior<Mob> {
+public class ApproachTargetIfCannotAim extends Behavior<LivingEntity> {
 
     private final Function<LivingEntity, Float> speedModifier;
 
-    public ApproachTargetAndAim(Function<LivingEntity, Float> speedModifier) {
+    public ApproachTargetIfCannotAim(Function<LivingEntity, Float> speedModifier) {
         super(ImmutableMap.of(
                 MemoryModuleType.WALK_TARGET, MemoryStatus.REGISTERED,
-                MemoryModuleType.LOOK_TARGET, MemoryStatus.REGISTERED,
                 MemoryModuleType.ATTACK_TARGET, MemoryStatus.VALUE_PRESENT,
                 ModBrainMemories.AIM_TICKS.get(), MemoryStatus.REGISTERED,
                 ModBrainMemories.WEAPON_IDEAL_RANGE.get(), MemoryStatus.VALUE_PRESENT,
@@ -39,61 +35,42 @@ public class ApproachTargetAndAim extends Behavior<Mob> {
         this.speedModifier = speedModifier;
     }
 
-    public ApproachTargetAndAim(float speedModifier) {
+    public ApproachTargetIfCannotAim(float speedModifier) {
         this(entity -> speedModifier);
     }
 
     @Override
-    protected boolean canStillUse(ServerLevel level, Mob mob, long gameTime) {
-        Brain<?> brain = mob.getBrain();
+    protected boolean canStillUse(ServerLevel level, LivingEntity entity, long gameTime) {
+        Brain<?> brain = entity.getBrain();
         return brain.hasMemoryValue(MemoryModuleType.ATTACK_TARGET)
                 && brain.hasMemoryValue(ModBrainMemories.WEAPON_IDEAL_RANGE.get())
                 && brain.hasMemoryValue(ModBrainMemories.WEAPON_MAX_RANGE.get());
     }
 
-    @Override
-    protected void stop(ServerLevel level, Mob mob, long gameTime) {
-        if (!this.timedOut(gameTime)) {
-            mob.getBrain().setMemory(ModBrainMemories.AIM_TICKS.get(), 0);
-        }
-    }
-
     @SuppressWarnings("OptionalGetWithoutIsPresent")  // because already handled on canStillUse
     @Override
-    protected void tick(ServerLevel level, Mob mob, long gameTime) {
-        Brain<?> brain = mob.getBrain();
+    protected void tick(ServerLevel level, LivingEntity entity, long gameTime) {
+        Brain<?> brain = entity.getBrain();
         LivingEntity target = brain.getMemory(MemoryModuleType.ATTACK_TARGET).get();
         float approachDist = brain.getMemory(ModBrainMemories.WEAPON_IDEAL_RANGE.get()).get();
         float weaponRange = brain.getMemory(ModBrainMemories.WEAPON_MAX_RANGE.get()).get();
         int aimTicks = brain.getMemory(ModBrainMemories.AIM_TICKS.get()).orElse(0);
-        boolean lineOfSight = mob.getSensing().hasLineOfSight(target);
 
-        if (!brain.hasMemoryValue(MemoryModuleType.LOOK_TARGET)) {
-            brain.setMemory(MemoryModuleType.LOOK_TARGET, new PatchedEntityTracker(target, true));
-        }
-
-        if (brain.hasMemoryValue(MemoryModuleType.WALK_TARGET)) {
-            aimTicks = 0;
-        } else {
-            aimTicks += lineOfSight ? 1 : -1;
-            aimTicks = Mth.clamp(aimTicks, MIN_AIM_TICKS, MAX_AIM_TICKS);
-        }
-        brain.setMemory(ModBrainMemories.AIM_TICKS.get(), aimTicks);
-
-        if (!mob.closerThan(target, weaponRange) || aimTicks <= MIN_AIM_TICKS) {
+        if (!entity.closerThan(target, weaponRange) || aimTicks <= MIN_AIM_TICKS) {
             if (!brain.hasMemoryValue(MemoryModuleType.WALK_TARGET)) {
                 brain.setMemory(MemoryModuleType.WALK_TARGET, new WalkTarget(
-                        new EntityTracker(target, false), this.speedModifier.apply(mob), 0
+                        new EntityTracker(target, false), this.speedModifier.apply(entity), 0
                 ));
             }
             return;
         }
 
-        if (mob.closerThan(target, approachDist) && lineOfSight) {
+        boolean lineOfSight = entity instanceof Mob mob
+                ? mob.getSensing().hasLineOfSight(target) : entity.hasLineOfSight(target);
+        if (entity.closerThan(target, approachDist) && lineOfSight) {
             if (brain.hasMemoryValue(MemoryModuleType.WALK_TARGET)) {
                 brain.eraseMemory(MemoryModuleType.WALK_TARGET);
             }
         }
     }
-
 }
