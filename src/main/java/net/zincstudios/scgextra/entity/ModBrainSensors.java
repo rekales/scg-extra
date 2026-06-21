ab249ccd858dbd2be2558f161f4801daac0b9f36
Krei <criketgun@gmail.com>
2026-06-22 01:23:48 +0800

    add custom player sensor

--- src/main/java/net/zincstudios/scgextra/entity/ModBrainSensors.java
+++ src/main/java/net/zincstudios/scgextra/entity/ModBrainSensors.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.zincstudios.scgextra.SCGExtra;
 import net.zincstudios.scgextra.entity.common.brain.CustomGunSensor;
 import net.zincstudios.scgextra.entity.common.brain.HeldGunSensor;
+import net.zincstudios.scgextra.entity.common.brain.VarRangePlayerSensor;
 
 import java.util.function.Supplier;
 
@@ -21,6 +22,16 @@ public class ModBrainSensors {
             .register("held_gun_rapid", () -> new SensorType<>(() -> new HeldGunSensor(1)));
     public static final Supplier<SensorType<CustomGunSensor>> CUSTOM_GUN = SENSOR_TYPES
             .register("custom_gun", () -> new SensorType<>(CustomGunSensor::new));
+
+    // Improved versions of vanilla sensors
+    public static final Supplier<SensorType<VarRangePlayerSensor>> SHORT_RANGE_PLAYER = SENSOR_TYPES
+            .register("short_range_player", () -> new SensorType<>(() -> new VarRangePlayerSensor(20, 16)));
+    public static final Supplier<SensorType<VarRangePlayerSensor>> MEDIUM_RANGE_PLAYER = SENSOR_TYPES
+            .register("medium_range_player", () -> new SensorType<>(() -> new VarRangePlayerSensor(30, 24)));
+    public static final Supplier<SensorType<VarRangePlayerSensor>> LONG_RANGE_PLAYER = SENSOR_TYPES
+            .register("long_range_player", () -> new SensorType<>(() -> new VarRangePlayerSensor(40, 32)));
+    public static final Supplier<SensorType<VarRangePlayerSensor>> VERY_LONG_RANGE_PLAYER = SENSOR_TYPES
+            .register("very_long_range_player", () -> new SensorType<>(() -> new VarRangePlayerSensor(50, 48)));
 
     public static void register(IEventBus modEventBus) {
         SENSOR_TYPES.register(modEventBus);
