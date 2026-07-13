ce0a79576f5f9b7e6e558ddf3e28d9cbd8b9debd
Daragetsu <nabbonoushad@gmail.com>
2026-07-14 00:33:39 +0600

    glowing eyes

--- src/main/java/net/zincstudios/scgextra/entity/neutral/overworld/inflicted_wolf/InflictedWolfRenderer.java
+++ src/main/java/net/zincstudios/scgextra/entity/neutral/overworld/inflicted_wolf/InflictedWolfRenderer.java
@@ -1,18 +1,46 @@
 package net.zincstudios.scgextra.entity.neutral.overworld.inflicted_wolf;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 
 import net.zincstudios.scgextra.SCGExtra;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class InflictedWolfRenderer extends GeoEntityRenderer<InflictedWolfEntity> {
 
     public InflictedWolfRenderer(Context context) {
         super(context, new InflictedWolfModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer<>(this){
+            @Override
+            public void render(PoseStack poseStack, InflictedWolfEntity animatable, BakedGeoModel bakedModel, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
+                RenderType emissiveRenderType = getRenderType(animatable);
+		        getRenderer().reRender(
+                    bakedModel,
+                    poseStack,
+                    bufferSource,
+                    animatable,
+                    emissiveRenderType,
+					bufferSource.getBuffer(emissiveRenderType),
+                    partialTick,
+                    LightTexture.FULL_BRIGHT,
+                    OverlayTexture.NO_OVERLAY,
+                    1,
+                    1,
+                    1,
+                    1
+                );
+            }
+        });
         this.shadowRadius = 0.8F;
     }
 
