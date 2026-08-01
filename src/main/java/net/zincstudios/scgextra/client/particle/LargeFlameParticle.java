abc7c3ec9656c1596854bd1063f1d439f99edd26
Daragetsu <nabbonoushad@gmail.com>
2026-08-01 21:04:48 +0600

    animated flames

--- src/main/java/net/zincstudios/scgextra/client/particle/LargeFlameParticle.java
+++ src/main/java/net/zincstudios/scgextra/client/particle/LargeFlameParticle.java
@@ -3,58 +3,29 @@ package net.zincstudios.scgextra.client.particle;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraft.util.Mth;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class LargeFlameParticle extends RisingParticle {
+//copy from Sonic Boom
+@OnlyIn(Dist.CLIENT)
+public class LargeFlameParticle extends HugeExplosionParticle {
+   protected LargeFlameParticle(ClientLevel level, double x, double y, double z, double quadSizeMultiplier, SpriteSet sprites) {
+      super(level, x, y, z, quadSizeMultiplier, sprites);
+      this.lifetime = 16;
+      this.quadSize = 0.5F;
+      this.setSpriteFromAge(sprites);
+   }
 
-    LargeFlameParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        super(level, x, y, z, xSpeed, ySpeed, zSpeed);
-        this.gravity = 0.015F + this.random.nextFloat() * -0.0F;
-    }
+   @OnlyIn(Dist.CLIENT)
+   public static class Provider implements ParticleProvider<SimpleParticleType> {
+      private final SpriteSet sprites;
 
-    public ParticleRenderType getRenderType() {
-        return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
-    }
+      public Provider(SpriteSet sprites) {
+         this.sprites = sprites;
+      }
 
-    public void move(double x, double y, double z) {
-        this.setBoundingBox(this.getBoundingBox().move(x, y, z));
-        this.setLocationFromBoundingbox();
-    }
-
-    public float getQuadSize(float scaleFactor) {
-        float f = ((float)this.age + scaleFactor) / (float)this.lifetime;
-        return this.quadSize * (2.0F - f * f * 0.5F);
-    }
-
-    public int getLightColor(float partialTick) {
-        float f = ((float)this.age + partialTick) / (float)this.lifetime;
-        f = Mth.clamp(f, 0.0F, 1.0F);
-        int i = super.getLightColor(partialTick);
-        int j = i & 255;
-        int k = i >> 16 & 255;
-        j += (int)(f * 15.0F * 16.0F);
-        if (j > 240) {
-            j = 240;
-        }
-
-        return j | k << 16;
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public static class Provider implements ParticleProvider<SimpleParticleType> {
-        private final SpriteSet sprite;
-
-        public Provider(SpriteSet sprites) {
-            this.sprite = sprites;
-        }
-
-        public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-            LargeFlameParticle flameParticle = new LargeFlameParticle(level, x, y, z, xSpeed, ySpeed, zSpeed);
-            flameParticle.pickSprite(this.sprite);
-            return flameParticle;
-        }
-    }
-
+      public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+         return new LargeFlameParticle(level, x, y, z, xSpeed, this.sprites);
+      }
+   }
 }
