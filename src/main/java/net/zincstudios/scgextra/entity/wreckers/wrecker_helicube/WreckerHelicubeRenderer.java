8475af08a72da4162060f2887f0622f876ff7e7b
Daragetsu <nabbonoushad@gmail.com>
2026-08-03 20:13:35 +0600

    wreckers glowmasks

--- src/main/java/net/zincstudios/scgextra/entity/wreckers/wrecker_helicube/WreckerHelicubeRenderer.java
+++ src/main/java/net/zincstudios/scgextra/entity/wreckers/wrecker_helicube/WreckerHelicubeRenderer.java
@@ -1,10 +1,19 @@
 package net.zincstudios.scgextra.entity.wreckers.wrecker_helicube;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.zincstudios.scgextra.SCGExtra;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class WreckerHelicubeRenderer extends GeoEntityRenderer<WreckerHelicubeEntity> {
 
@@ -33,6 +42,27 @@ public class WreckerHelicubeRenderer extends GeoEntityRenderer<WreckerHelicubeEn
             }
         });
         this.shadowRadius = 0.4F;
+        this.addRenderLayer(new AutoGlowingGeoLayer<>(this){
+            @Override
+            public void render(PoseStack poseStack, WreckerHelicubeEntity animatable, BakedGeoModel bakedModel, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
+                RenderType emissiveRenderType = getRenderType(animatable);
+		        getRenderer().reRender(
+                    bakedModel,
+                    poseStack,
+                    bufferSource,
+                    animatable,
+                    emissiveRenderType,
+					bufferSource.getBuffer(emissiveRenderType),
+                    partialTick,
+                    LightTexture.FULL_BRIGHT,
+                    OverlayTexture.NO_OVERLAY,
+                    1,
+                    1,
+                    1,
+                    1
+                );
+            }
+        });
     }
 
     @Override
--- src/main/java/net/zincstudios/scgextra/entity/wreckers/wrecker_turret/WreckerTurretRenderer.java
+++ src/main/java/net/zincstudios/scgextra/entity/wreckers/wrecker_turret/WreckerTurretRenderer.java
@@ -1,10 +1,18 @@
 package net.zincstudios.scgextra.entity.wreckers.wrecker_turret;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.zincstudios.scgextra.SCGExtra;
 import net.zincstudios.scgextra.entity.common.client.GunnerRenderer;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class WreckerTurretRenderer extends GunnerRenderer<WreckerTurretEntity> {
     private static final double PILLAR_OFFSET_Z = 12.5D / 16D;
@@ -12,6 +20,27 @@ public class WreckerTurretRenderer extends GunnerRenderer<WreckerTurretEntity> {
     public WreckerTurretRenderer(EntityRendererProvider.Context context) {
         super(context, new DefaultedEntityGeoModel<>(SCGExtra.asResource("wreckers/wrecker_turret")));
         this.noDeathTilt();
+        this.addRenderLayer(new AutoGlowingGeoLayer<>(this){
+            @Override
+            public void render(PoseStack poseStack, WreckerTurretEntity animatable, BakedGeoModel bakedModel, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
+                RenderType emissiveRenderType = getRenderType(animatable);
+		        getRenderer().reRender(
+                    bakedModel,
+                    poseStack,
+                    bufferSource,
+                    animatable,
+                    emissiveRenderType,
+					bufferSource.getBuffer(emissiveRenderType),
+                    partialTick,
+                    LightTexture.FULL_BRIGHT,
+                    OverlayTexture.NO_OVERLAY,
+                    1,
+                    1,
+                    1,
+                    1
+                );
+            }
+        });
     }
 
     @Override
