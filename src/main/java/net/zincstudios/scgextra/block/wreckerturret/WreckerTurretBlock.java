4b6bef8ead090898c48a5da54082c442c6930c79
Daragetsu <nabbonoushad@gmail.com>
2026-08-05 00:35:37 +0600

    face opposite to players

--- src/main/java/net/zincstudios/scgextra/block/wreckerturret/WreckerTurretBlock.java
+++ src/main/java/net/zincstudios/scgextra/block/wreckerturret/WreckerTurretBlock.java
@@ -106,7 +106,7 @@ public class WreckerTurretBlock extends BaseEntityBlock {
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
     }
 
     @Override
