25521ca7fe636bf0b3aeba53779dbdc38ef2b36d
Krei <criketgun@gmail.com>
2026-08-11 15:32:28 +0800

    add single use desc to rpg

--- src/main/java/net/zincstudios/scgextra/item/WreckerRpgItem.java
+++ src/main/java/net/zincstudios/scgextra/item/WreckerRpgItem.java
@@ -1,14 +1,18 @@
 package net.zincstudios.scgextra.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.zincstudios.scgextra.SCGExtra;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
@@ -22,9 +26,10 @@ import software.bernie.geckolib.util.GeckoLibUtil;
 import top.ribs.scguns.common.Gun;
 import top.ribs.scguns.item.GunItem;
 
+import java.util.List;
 import java.util.function.Consumer;
 
-// TODO: proper
+// TODO: proper gun implementation
 @MethodsReturnNonnullByDefault
 public class WreckerRpgItem extends GunItem implements GeoItem {
 
@@ -33,6 +38,12 @@ public class WreckerRpgItem extends GunItem implements GeoItem {
 
     public WreckerRpgItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, worldIn, tooltip, flag);
+        tooltip.add(Component.translatable("item." + SCGExtra.MOD_ID + ".single_use_desc").withStyle(ChatFormatting.GOLD));
     }
 
     @Override
